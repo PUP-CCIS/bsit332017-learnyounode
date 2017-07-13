b30f0e74530754a094de6a72ce7fabe9275d0e25
jonalynfe <jonalynfeebrada11@gmail.com>
2017-07-13 23:29:33 +0800

    2
    2

---
+++ Ebrada/fileserver.js
@@ -0,0 +1,9 @@
+async function logFetch(url) {
+  try {
+    const response = await fetch(url);
+    console.log(await response.text());
+  }
+  catch (err) {
+    console.log('fetch failed', err);
+  }
+}
