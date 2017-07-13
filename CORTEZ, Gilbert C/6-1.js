bb5fd2515f367b17dfebf50d5d66ca9f3ca87675
Gilbert Critica Cortez <gilbert230709@gmail.com>
2017-07-13 09:37:37 -0700

    Update 6-1.js

--- CORTEZ, Gilbert C/6-1.js
+++ CORTEZ, Gilbert C/6-1.js
@@ -1,14 +1,4 @@
-var lslib = require('./6-2');
-
-var dirname = process.argv[2];
-var ext = process.argv[3];
-
-lslib(dirname, ext, function(err, files) {
-  for (i = 0; i < files.length; i++) {
-    console.log(files[i]);
-  }
-});
-/*var myFile=require('./6-2.js');
+var myFile=require('./6-2.js');
 var directory=process.argv[2];
 var extension=process.argv[3];
 myFile(directory,extension, function(err,response){
@@ -17,5 +7,5 @@ myFile(directory,extension, function(err,response){
 	}
 });
 	
-*/
+
 
